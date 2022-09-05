2de8249cc1a432092f77989657ab9d833b9c2190
Siarhei Prymakovich <prima.psa@gmail.com>
2022-09-05 17:44:53 +0300

    refactor: remove unused import

--- src/Audiocall/components/Description.tsx
+++ src/Audiocall/components/Description.tsx
@@ -1,14 +1,13 @@
-import React from 'react';
 import style from '../AudioCall.module.css';
 import { Twords } from '../../types';
-const Description = (props: {words: Twords}) => {
-    const { words }  = props;
-    return (
-        <div className={style.description}>
-            <div className={style.description__text}>{words.word}</div>
-            <div className={style.description__text}>{words.transcription}</div>
-        </div>
-    )
-}
+const Description = (props: { words: Twords }) => {
+  const { words } = props;
+  return (
+    <div className={style.description}>
+      <div className={style.description__text}>{words.word}</div>
+      <div className={style.description__text}>{words.transcription}</div>
+    </div>
+  );
+};
 
 export default Description;
