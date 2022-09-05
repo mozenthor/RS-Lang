5679c039b436400c9ed44c8355138ec3e89d264a
Siarhei Prymakovich <prima.psa@gmail.com>
2022-09-05 19:57:29 +0300

    refactor: change key code

--- src/Audiocall/data/default.ts
+++ src/Audiocall/data/default.ts
@@ -1,5 +1,5 @@
 import { Tstats } from "../../types";
 const stat: Tstats = { correct: [], incorrect: [], maxtry: 0 };
 export const levels = ['A1','A2','B1','B2','C1','C2'];
-export const ENTER_CODE = 13;
+export const ENTER_CODE = 'Enter';
 export default stat;
