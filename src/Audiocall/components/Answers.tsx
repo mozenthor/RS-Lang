3a065d8fb2f4e74f2d222af44c7d566a5498de28
Siarhei Prymakovich <prima.psa@gmail.com>
2022-09-05 17:42:43 +0300

    feat: implement key binding

--- src/Audiocall/components/Answers.tsx
+++ src/Audiocall/components/Answers.tsx
@@ -2,40 +2,66 @@ import React, { useState, useEffect } from 'react';
 import { Twords } from '../../types';
 
 import style from '../AudioCall.module.css';
-const Answers = (props: { words: Twords, cb: any, description: any}) => {
+const Answers = (props: { words: Twords; cb: any; description: any }) => {
   const answers = props.words.answers;
   const [currentAnswer, setCurrentAnswer] = useState('');
   const [isAnswered, setAnswered] = useState(false);
   const correctAnswer = props.words.wordTranslate;
+
   useEffect(() => {
     setAnswered(false);
   }, [answers]);
 
-  const answerHandler = (clickEvent: React.MouseEvent<HTMLElement>) => {
-    const answer = (clickEvent.target as HTMLButtonElement).value;
+  const handleAnswer = (answer: string) => {
     setCurrentAnswer(answer);
-    props.cb(answer);   
+    props.cb(answer);
     setAnswered(true);
     props.description();
   };
 
-  const buttons = answers.map((value: string) => {
+  const answerHandler = (clickEvent: React.MouseEvent<HTMLElement>) => {
+    const answer = (clickEvent.target as HTMLButtonElement).value;
+    handleAnswer(answer);
+  };
+
+  const buttons = answers.map((value: string, ind: number) => {
     let coloredAnswer = '';
     if (isAnswered) {
       coloredAnswer = value === correctAnswer ? style.succeses : '';
       coloredAnswer = currentAnswer === value && currentAnswer !== correctAnswer ? style.wrong : coloredAnswer;
     }
+
     return (
       <button
         className={style.btn + ' ' + coloredAnswer}
         value={value}
         disabled={isAnswered}
         onClick={answerHandler}
-        key={value}>
+        key={value}
+      >
         {value}
+        <span className={style.btn__hint}>{ind + 1}</span>
       </button>
     );
   });
+
+  const keyboardHandler = (e: any): void => {
+    const keys = ['Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5'];
+    const code = e.code;
+    const isAllowed = keys.indexOf(code);
+
+    if (isAllowed >= 0) {
+      const answer = answers[isAllowed];
+      handleAnswer(answer);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('keydown', keyboardHandler);
+    return () => {
+      window.removeEventListener('keydown', keyboardHandler);
+    };
+  }, [answers]);
 
   return <div className={style.answers}>{buttons}</div>;
 };
